e2b9d6b9ee1d9b20d92334829ad8d4698b389a6e
krisvanneste <kris.vanneste@oma.be>
2018-12-10 15:04:15 +0000

    Added from __future__ stanza. Split long lines over multiple lines. Moved content in __main__ section to separate script in app folder.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/eqcatalog/trunk@4838 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- HY4.py
+++ HY4.py
@@ -2,6 +2,9 @@
 SeismicEruption HY4 earthquake catalog format
 """
 
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+
 import ctypes
 import struct
 
@@ -21,11 +24,13 @@ class HYPDAT(ctypes.Structure):
 				("reserved3", ctypes.c_byte)]
 
 	def pack(self):
-		return struct.pack("2ih2B2hi4B", self.latitude, self.longitude, self.year, self.month, self.day, self.minutes, self.tseconds, self.depth, self.magnitude, 0, 0, 0)
+		return struct.pack("2ih2B2hi4B", self.latitude, self.longitude, self.year,
+							self.month, self.day, self.minutes, self.tseconds,
+							self.depth, self.magnitude, 0, 0, 0)
 
 
 def readHY4(filespec, Mtype='ML'):
-	from eqcatalog import EQCatalog
+	from .eqcatalog import EQCatalog
 	catalog = EQCatalog.from_HY4(filespec, Mtype)
 	return catalog
 
@@ -36,25 +41,3 @@ def writeHY4(filespec, catalog):
 		hyp = eq.to_HY4()
 		ofd.write("%s" % hyp.pack())
 	ofd.close()
-
-
-
-if __name__ == "__main__":
-	import os
-	import datetime
-	import seismodb
-
-	out_folder = r"D:\GIS-data\KSB-ORB"
-	#out_folder = r"E:\Home\_kris\Meetings\2018 - Opendeurdagen"
-	#out_folder = r"C:\Program Files (x86)\SeismicEruption\OpenDoorDays"
-
-	filespec = os.path.join(out_folder, "ROB.HY4")
-
-	region = (0,8,49,52)
-	start_date = datetime.date(1985, 1, 1)
-	#end_date = datetime.date(2007, 10, 1)
-	end_date = datetime.datetime.now()
-	min_mag = 0.0
-	max_mag = 7.0
-	catalog = seismodb.query_ROB_LocalEQCatalog(region, start_date=start_date, end_date=end_date, Mmin=min_mag, Mmax=max_mag)
-	catalog.export_HY4(filespec)
