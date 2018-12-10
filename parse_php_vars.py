7d417d0bde1d498a2fa0e3ac38519e6469292e39
krisvanneste <kris.vanneste@oma.be>
2018-12-10 15:16:31 +0000

    Fixed reading of php files in parse_php_vars function to make it python3-compatible.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/eqcatalog/trunk@4842 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- parse_php_vars.py
+++ parse_php_vars.py
@@ -1,7 +1,11 @@
-"""
-Parse variables from PHP file.
+# -*- coding: iso-Latin-1 -*-
 
 """
+Parse variables from PHP file.
+"""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
 
 def parse_php_vars(php_file):
 	"""
@@ -13,8 +17,9 @@ def parse_php_vars(php_file):
 	:return:
 		dict, mapping variable names (with leading '$') to values
 	"""
-	with open(php_file) as fp:
+	with open(php_file, 'rb') as fp:
 		php = fp.read()
+	php = php.decode('Latin-1')
 
 	php_lines = [line for line in php.split(';')]
 	php_vars= {}
@@ -36,10 +41,10 @@ def parse_php_vars(php_file):
 
 
 if __name__ == "__main__":
-	php_file = r"E:\Home\_kris\Python\seismo\eqcatalog\webenq\const_inqEN.php"
+	php_file = "E:\\Home\\_kris\\Python\\seismo\\eqcatalog\\webenq\\const_inqFR.php"
 	php_vars = parse_php_vars(php_file)
 	#for var, val in php_vars.items():
 	#	print("%s: %s" % (var, val))
-	#print php_vars['$form23']
+	#print(php_vars['$form23'])
 	for item in sorted(php_vars.items()):
 		print(item)
