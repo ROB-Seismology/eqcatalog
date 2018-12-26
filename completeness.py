203be6109b03f373f7951583f59af88bb1107932
krisvanneste <kris.vanneste@oma.be>
2018-12-26 16:47:27 +0000

    Removed ROB-specific completeness objects, which are now in rob.completeness.py.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/eqcatalog/trunk@4903 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- completeness.py
+++ completeness.py
@@ -263,21 +263,3 @@ class Completeness:
 			table[-1,0] = self.min_years.min()
 			table[-1,1] = Mmax
 		return table
-
-
-## NOTE: I think threshold magnitudes should be a multiple of dM (or dM/2)!
-#Completeness_Leynaud = Completeness([1350, 1911, 1985], [4.75, 3.25, 1.75], "MS")
-Completeness_Leynaud = Completeness([1350, 1911, 1985],
-									[4.7, 3.3, 1.8], "MS")
-
-Completeness_Rosset = Completeness([1350, 1926, 1960, 1985],
-									[5.0, 4.0, 3.0, 1.8], "MS")
-
-## Following relation is for MW based on conversion from ML using Ahorner (1983)
-Completeness_MW_201303a = Completeness([1350, 1750, 1860, 1905, 1960, 1985],
-										[5.2, 4.9, 4.5, 4.0, 3.0, 2.2], "MW")
-## Following relation is for MW based on conversion from ML using Reamer and Hinzen (2004)
-Completeness_MW_201303b = Completeness([1350, 1750, 1860, 1905, 1960, 1985],
-										[5.2, 4.9, 4.5, 3.9, 2.9, 2.0], "MW")
-
-DEFAULT_COMPLETENESS = Completeness_MW_201303a
