f5547daf17d25dda0f55bc8c14caa2eb0a093ebe
krisvanneste <kris.vanneste@oma.be>
2018-12-26 22:45:48 +0000

    Moved HY4.py and parse_php_vars to io submodule.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/eqcatalog/trunk@4916 40b490c5-b4d9-47cb-8714-9bef99b524d5

---
+++ io/__init__.py
@@ -0,0 +1,48 @@
+"""
+Input/output of earthquake catalogs
+"""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+
+## Reloading mechanism
+try:
+	reloading
+except NameError:
+	## Module is imported for the first time
+	reloading = False
+else:
+	## Module is reloaded
+	reloading = True
+	try:
+		## Python 3
+		from importlib import reload
+	except ImportError:
+		## Python 2
+		pass
+
+
+__all__ = ["HY4", "read_named_catalog", "read_catalog_sql", "read_catalog_csv",
+			"read_catalog_gis"]
+
+
+## Import submodules
+
+## HY4
+if not reloading:
+	from . import HY4
+else:
+	reload(HY4)
+
+## read_catalog (depends on ..eqrecord, ..eqcatalog, ..time_functions_np)
+if not reloading:
+	from . import read_catalog
+else:
+	reload(read_catalog)
+from .read_catalog import *
+
+## parse_php_vars
+if not reloading:
+	from . import parse_php_vars
+else:
+	reload(parse_php_vars)
