a89e140859097d7cd2b5cc759480cbd23521d97e
krisvanneste <kris.vanneste@oma.be>
2018-12-26 10:55:07 +0000

    Added rob submodule. Added rob/completeness.py with ROB-specific completeness definitions.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/eqcatalog/trunk@4901 40b490c5-b4d9-47cb-8714-9bef99b524d5

---
+++ rob/__init__.py
@@ -0,0 +1,47 @@
+"""
+ROB-specific submodule of eqcatalog
+"""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+
+## Reloading mechanism
+try:
+	reloading
+except NameError:
+	## Module is imported for the first time
+	reloading = False
+else:
+	## Module is reloaded
+	reloading = True
+	try:
+		## Python 3
+		from importlib import reload
+	except ImportError:
+		## Python 2
+		pass
+
+
+## Import submodules
+
+## completeness (no internal dependencies)
+if not reloading:
+	from . import completeness
+else:
+	reload(completeness)
+from .completeness import *
+
+"""
+## source_models (no internal dependencies)
+if not reloading:
+	from . import source_models
+else:
+	reload(source_models)
+from .source_models import (read_source_model, rob_source_models_dict)
+
+## seismodb (depends on eqrecord, macrorecord, eqcatalog)
+if not reloading:
+	from . import seismodb
+else:
+	reload(seismodb)
+"""
