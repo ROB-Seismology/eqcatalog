a45d69fe91255c1da418c692ef5401f41f30eed7
krisvanneste <kris.vanneste@oma.be>
2019-03-13 16:05:34 +0000

    Reorganized plot_macroseismic_map.py script.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/eqcatalog/trunk@5184 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- app/plot_macroseismic_map.py
+++ app/plot_macroseismic_map.py
@@ -10,7 +10,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import os
 
 import mapping.layeredbasemap as lbm
-from eqcatalog.plot import plot_web_macroseismic_map, plot_official_macroseismic_map
+from eqcatalog.macro import get_aggregated_info_official, get_aggregated_info_web
 
 
 ## Earthquake to plot
@@ -19,9 +19,81 @@ from eqcatalog.plot import plot_web_macroseismic_map, plot_official_macroseismic
 ## 1938 earthquake
 #id_earth = 509
 ## 2002 Alsdorf
-id_earth = 1306
+#id_earth = 1306
 ## Kinrooi 25/05/2018
-#id_earth = 6625
+id_earth = 6625
+
+## Macroseismic paramerers
+enq_type = 'online'
+#enq_type = 'official'
+
+## Common
+min_fiability = 80
+
+## DYFI
+query_info = "cii"
+#query_info = "num_replies"
+min_replies = 3
+#filter_floors = (-100, 900)
+filter_floors = (0, 4)
+agg_method = 'mean'
+fix_records = True
+include_other_felt = True
+include_heavy_appliance = False
+remove_outliers = (0, 100)
+
+## Official enquiries
+min_or_max = 'mean'
+agg_function = 'average'
+
+## How to aggregate
+## No aggregation = plot at individual locations (if geocoded)
+#aggregate_by = None
+## Aggregate by commune or main commune
+aggregate_by = 'commune'
+#aggregate_by = 'main commune'
+## Aggregate by grid cell
+#aggregate_by = 'grid_5'
+
+
+if enq_type in ('internet', 'online'):
+	macro_info = get_aggregated_info_web(id_earth, min_replies=min_replies,
+				query_info=query_info, min_fiability=min_fiability,
+				filter_floors=filter_floors, aggregate_by=aggregate_by,
+				agg_method=agg_method, fix_records=fix_records,
+				include_other_felt=include_other_felt,
+				include_heavy_appliance=include_heavy_appliance,
+				remove_outliers=remove_outliers)
+else:
+	macro_info = get_aggregated_info_official(id_earth, min_fiability=min_fiability,
+				min_or_max=min_or_max, aggregate_by=aggregate_by,
+				agg_function=agg_function, min_val=1)
+
+
+plot_info = 'intensity'
+#plot_info = 'num_replies'
+
+
+## Choose symbols/polygons for aggregation by commune or main commune
+symbol_style = lbm.PointStyle(shape='D', size=5)
+#symbol_style = None
+
+## Color options
+colorbar_style = "default"
+color_gradient = "discrete"
+#color_gradient = "continuous"
+cmap = "rob"
+#cmap = "usgs"
+#cmap = "jet"
+
+thematic_num_replies = True
+
+## Extras
+#radii = [10, 25, 50]
+radii = []
+#plot_pie = 'asleep'
+plot_pie = None
+
 
 ## Map parameters
 region = (2, 7, 49.25, 51.75)
@@ -29,41 +101,6 @@ region = (2, 7, 49.25, 51.75)
 projection = "tmerc"
 graticule_interval = (2, 1)
 #graticule_interval = (0.5, 1)
-
-## DYFI parameters
-query_info = "cii"
-#query_info = "num_replies"
-min_replies = 3
-#filter_floors = (-100, 900)
-filter_floors = (0, 4)
-min_fiability = 20
-recalc = False
-
-## How to aggregate
-## No aggregation = plot at individual locations (if geocoded)
-#aggregate_by = None
-## Aggregate by commune or main commune
-#aggregate_by = 'commune'
-aggregate_by = 'main commune'
-## Aggregate by grid cell
-#aggregate_by = 'grid_5'
-
-## Choose symbols/polygons for aggregation by commune or main commune
-#symbol_style = lbm.PointStyle(shape='D', size=5)
-symbol_style = None
-
-## Color options
-color_gradient = "discrete"
-#color_gradient = "continuous"
-cmap = "rob"
-#cmap = "usgs"
-#cmap = "jet"
-
-## Extras
-#radii = [10, 25, 50]
-radii = []
-#plot_pie = 'asleep'
-plot_pie = None
 
 
 ## Title and output file
@@ -77,20 +114,20 @@ fig_filespec = None
 
 
 ## Plot
-"""
-plot_web_macroseismic_map(id_earth, region=region, projection=projection,
-				graticule_interval=graticule_interval, min_replies=min_replies,
-				query_info=query_info, min_fiability=min_fiability, filter_floors=filter_floors,
-				cmap=cmap, color_gradient=color_gradient, symbol_style=symbol_style,
-				aggregate_by=aggregate_by, radii=radii,
-				plot_pie=plot_pie, title=title, fig_filespec=fig_filespec,
-				recalc=recalc)
-"""
-
-plot_official_macroseismic_map(id_earth, region=region, projection=projection,
-				graticule_interval=graticule_interval, min_fiability=min_fiability,
-				min_or_max='min', aggregate_by=aggregate_by, agg_function='average',
-				int_conversion='round', symbol_style=symbol_style, cmap=cmap,
-				color_gradient=color_gradient, radii=radii, title=title,
+if macro_info:
+	region = "auto"
+	macro_info.plot_map(region=region, projection=projection,
+				graticule_interval=graticule_interval, plot_info=plot_info,
+				int_conversion='round', symbol_style=symbol_style,
+				thematic_num_replies=thematic_num_replies, cmap=cmap,
+				color_gradient=color_gradient, admin_level='province',
+				colorbar_style=colorbar_style, radii=radii, title=title,
 				fig_filespec=fig_filespec, verbose=True)
 
+	#print(macro_info_coll.to_geojson())
+
+	#gis_file = os.path.splitext(fig_filespec)[0] + ".TAB"
+	#macro_info_coll.export_gis('MapInfo File', gis_file)
+
+	#geotiff_file = os.path.splitext(fig_filespec)[0] + ".TIF"
+	#macro_info_coll.export_geotiff(geotiff_file)
