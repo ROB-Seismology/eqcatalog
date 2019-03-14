f52cc9d4b72acce4c5cd70c070c3ea10b2e16125
krisvanneste <kris.vanneste@oma.be>
2019-03-14 13:22:29 +0000

    Renamed export_emec.py to export_catalog.py, and added some improvements.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/eqcatalog/trunk@5192 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- app/export_catalog.py
+++ app/export_catalog.py
@@ -8,18 +8,37 @@ end_date = 2018
 region = (1, 8, 49, 52)
 
 ## Gruenthal
-#start_date = 1985
-#Mmin = 1.0
+"""
+start_date = 1985
+Mmin = 1.0
 #event_type = 'ke'
-#event_type = 'ki'
+event_type = 'ki'
+"""
 
 ## Musson
+"""
 start_date = 1910
 Mmin = 3.0
 event_type = 'ke'
+"""
+
+## Tractebel
+start_date = 1350
+region = (0, 8, 49, 52)
+event_type = 'ke'
+Mmin = 1.8
+Mtype = 'MW'
+Mrelation = 'default'
 
 raw_cat = eqcatalog.rob.query_local_eq_catalog(start_date=start_date, end_date=end_date,
-											region=region, Mmin=Mmin, event_type=event_type)
+											region=region, Mmin=0, event_type=event_type)
+
+## Add converted moment magnitude column
+moment_mags = raw_cat.get_magnitudes(Mtype, Mrelation)
+for i in range(len(raw_cat)):
+	raw_cat[i].set_mag('MWc', moment_mags[i])
+raw_cat = raw_cat.subselect(Mmin=Mmin, Mtype='MWc', Mrelation={})
+
 raw_cat.print_info()
 cat = raw_cat.get_sorted()
 #cat1 = raw_cat.subselect(Mmin=Mmin, Mtype='ML', Mrelation={})
@@ -31,7 +50,8 @@ cat = raw_cat.get_sorted()
 
 csv_file = "C:\\Temp\\ROB_catalog_%s_%d-%d.csv" % (event_type, start_date, end_date)
 columns = ['ID', 'date', 'time', 'name', 'lon', 'lat', 'depth',
-			'ML', 'MS', 'MW', 'intensity_max', 'errt', 'errh', 'errz', 'errM']
-#cat.export_csv(csv_file, columns=columns)
+			'ML', 'MS', 'MW', 'MWc', 'intensity_max',
+			'errt', 'errh', 'errz', 'errM']
+cat.export_csv(csv_file, columns=columns)
 
 cat.plot_map()
