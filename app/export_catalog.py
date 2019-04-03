f73e2c2e7459528ad2f9c417bb18ab467c484432
krisvanneste <kris.vanneste@oma.be>
2019-04-03 14:49:42 +0000

    Added selection criteria for David Baumont.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/eqcatalog/trunk@5274 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- app/export_catalog.py
+++ app/export_catalog.py
@@ -23,21 +23,36 @@ event_type = 'ke'
 """
 
 ## Tractebel
+"""
 start_date = 1350
 region = (0, 8, 49, 52)
 event_type = 'ke'
 Mmin = 1.8
 Mtype = 'MW'
-Mrelation = 'default'
+Mrelation = {}
+"""
+
+## David Baumont
+start_date = 1350
+region = (0, 8, 49, 52)
+event_type = 'ke'
+Mmin = 1.0
+Mtype = 'MW'
+Mrelation = {}
+
 
 raw_cat = eqcatalog.rob.query_local_eq_catalog(start_date=start_date, end_date=end_date,
 											region=region, Mmin=0, event_type=event_type)
 
-## Add converted moment magnitude column
+## Add converted moment magnitude column for subselecting
 moment_mags = raw_cat.get_magnitudes(Mtype, Mrelation)
 for i in range(len(raw_cat)):
 	raw_cat[i].set_mag('MWc', moment_mags[i])
 raw_cat = raw_cat.subselect(Mmin=Mmin, Mtype='MWc', Mrelation={})
+
+## Remove Mwc column again
+#for eq in raw_cat:
+#	del eq.mag['MWc']
 
 raw_cat.print_info()
 cat = raw_cat.get_sorted()
