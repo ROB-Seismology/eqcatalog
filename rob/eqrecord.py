3033756314cbad86415d03dd9398c989a1887bee
krisvanneste <kris.vanneste@oma.be>
2019-04-08 15:09:09 +0000

    Renamed get_macroseismic_data_aggregated_web method of ROBLocalEarthquake to get_aggregated_online_macro_info, removed 'min_val' argument, replaced 'group_by_main_village' argument with 'aggregate_by', and added 'fix_records', 'include_other_felt', 'include_heavy_appliance', 'fix_records' and 'remove_outliers' arguments. Renamed get_macroseismic_data_aggregated_official method of ROBLocalEarthquake to get_aggregated_traditional_macro_info, removed 'min_val' argument, replaced 'group_by_main_village' argument with 'aggregate_by', and added 'id_com' and 'data_type' arguments. Added get_aggregated_historical_macro_info and get_aggregated_official_macro_info methods to ROBLocalEarthquake. Added get_traditional_macroseismic_info, get_historical_macroseismic_info, get_official_macroseismic_info and get_isoseismal_macroseismic_info methods to ROBLocalEarthquake. Renamed get_macroseismic_enquiries and get_num_macroseismic_enquiries methods of ROBLocalEarthquake to get_online_macro_enquiries and get_num_online_macro_enquiries, respectively. Renamed get_Imax_web method of ROBLocalEarthquake to get_Imax_online, and modified arguments similar to get_aggregated_online_macro_info method. Renamed get_Imax_official method of ROBLocalEarthquake to get_Imax_traditional, and modified arguments similar to get_macroseismic_data_aggregated_official method. Added get_Imax_historical, get_Imax_official and get_Imax_isoseismal methods to ROBLocalEarthquake. Updated arguments of get_Imax method of ROBLocalEarthquake to match get_Imax_online and get_Imax_traditional methods.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/eqcatalog/trunk@5293 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- rob/eqrecord.py
+++ rob/eqrecord.py
@@ -36,9 +36,11 @@ class ROBLocalEarthquake(LocalEarthquake):
 		hash = hi.encode(self.ID)
 		return hash
 
-	def get_macroseismic_data_aggregated_web(self, min_replies=3, query_info="cii",
-					min_val=1, min_fiability=20.0, group_by_main_village=False,
-					filter_floors=False, agg_method="mean", verbose=False):
+	def get_aggregated_online_macro_info(self, min_replies=3, query_info="cii",
+					min_fiability=20.0, filter_floors=(0,4),
+					aggregate_by='commune', agg_method="mean", fix_records=True,
+					include_other_felt=True, include_heavy_appliance=False,
+					remove_outliers=(2.5, 97.5), verbose=False):
 		"""
 		Get online macroseismic information (possibly aggregated by
 		main commune) for this earthquake
@@ -49,75 +51,208 @@ class ROBLocalEarthquake(LocalEarthquake):
 		:param query_info:
 			str, either "cii", "cdi" or "mi"
 			(default: "cii")
-		:param min_val:
-			float, minimum intensity to return
-			(default: 1)
 		:param min_fiability:
 			float, minimum fiability of enquiry
 			(default: 20.)
-		:param group_by_main_village:
-			bool, whether or not to aggregate the results by main village
-			(default: False)
 		:param filter_floors:
 				(min_floor, max_floor) tuple, floors outside this range
 				(basement floors and upper floors) are filtered out
-				(default: False)
+				(default: (0, 4))
+		:param aggregate_by:
+			str, type of aggregation, specifying how macroseismic data should
+			be aggregated in the map, one of:
+			- 'id_com' or 'commune'
+			- 'id_main' or 'main commune'
+			- 'grid_X' (where X is grid spacing in km)
+			- None or '' (= no aggregation, i.e. all replies are
+			plotted individually (potentially on top of each other)
+			(default: 'commune')
 		:param agg_method:
-			str, aggregation function to use, one of "minimum", "maximum" or
-			"average"/"mean". If :param:`group_by_main_village` is False,
-			aggregation applies to the enquiries within a given (sub)commune.
+			str, how to aggregate individual enquiries,
+			either 'mean' (= ROB practice) or 'aggregated' (= DYFI practice)
+			(default: 'mean')
+		:param fix_records:
+			bool, whether or not to fix various issues (see :meth:`fix_all`)
+			(default: True)
+		:param include_other_felt:
+			bool, whether or not to take into acoount the replies to the
+			question "Did others nearby feel the earthquake ?"
+			(default: True)
+		:param include_heavy_appliance:
+			bool, whether or not to take heavy_appliance into account
+			as well (not standard, but occurs with ROB forms)
+			(default: False)
+		:param remove_outliers:
+			(min_pct, max_pct) tuple, percentile range to use.
+			Only applies if :param:`agg_method` = 'mean'
+			and if :param:`agg_info` = 'cii'
+			(default: 2.5, 97.5)
+		:param verbose:
+			bool, if True the query string will be echoed to standard output
+
+		:return:
+			instance of :class:`MacroInfoCollection`
+		"""
+		kwargs = locals().copy()
+		kwargs.pop('self')
+		#from .seismodb import query_online_macro_catalog_aggregated
+		#return query_online_macro_catalog_aggregated(self.ID, min_replies=min_replies,
+		#			query_info=query_info, min_val=min_val, min_fiability=min_fiability,
+		#			group_by_main_commune=group_by_main_commune, filter_floors=filter_floors,
+		#			agg_method=agg_method, verbose=verbose)
+		from ..macro import aggregate_online_macro_info
+		return aggregate_online_macro_info(self.ID, **kwargs)
+
+	def get_aggregated_traditional_macro_info(self, id_com=None, data_type='',
+			min_or_max='max', min_fiability=20, aggregate_by="commune",
+			agg_method="mean", verbose=False):
+		"""
+		Get traditional (historical / official) macroseismic information
+		(possibly aggregated by main commune) for this earthquake
+
+		:param id_com:
+			int, commune ID
+			or list of ints
+			(default: None)
+		:param data_type:
+			str, type of macroseismic data: '', 'official' or 'historical'
+			(default: '')
+		:param min_or_max:
+			str, one of 'min', 'mean' or 'max' to select between
+			intensity_min and intensity_max values in database
+			(default: 'max')
+		:param min_fiability:
+			float, minimum fiability of enquiry
+			(default: 20.)
+		:param aggregate_by:
+			str, type of aggregation, specifying how macroseismic data should
+			be aggregated in the map, one of:
+			- 'id_com' or 'commune'
+			- 'id_main' or 'main commune'
+			(default: 'commune')
+		:param agg_method:
+			str, aggregation function to use if :param:`aggregate_by`
+			is 'main commune', one of "min(imum)", "max(imum)", "median'
+			or "average"/"mean"
 			(default: "mean")
 		:param verbose:
 			Bool, if True the query string will be echoed to standard output
 
 		:return:
-			dict mapping commune IDs to instances of :class:`MacroseismicInfo`
+			instance of :class:`MacroInfoCollection`
 		"""
-		from .seismodb import query_web_macro_catalog
-		return query_web_macro_catalog(self.ID, min_replies=min_replies,
-					query_info=query_info, min_val=min_val, min_fiability=min_fiability,
-					group_by_main_village=group_by_main_village, filter_floors=filter_floors,
-					agg_method=agg_method, verbose=verbose)
+		kwargs = locals().copy()
+		kwargs.pop('self')
+		#from .seismodb import query_traditional_macro_catalog_aggregated
+		#return query_traditional_macro_catalog_aggregated(self.ID, **kwargs)
+		from ..macro import aggregate_traditional_macro_info
+		return aggregate_traditional_macro_info(self.ID, **kwargs)
 
-	def get_macroseismic_data_aggregated_official(self, min_or_max='max', min_val=1,
-			group_by_main_village=False, agg_method="mean", min_fiability=20,
+	def get_aggregated_official_macro_info(self, id_com=None, min_or_max='max',
+			group_by_main_commune=False, agg_method="mean", min_fiability=20,
 			verbose=False):
 		"""
 		Get official macroseismic information (possibly aggregated by
-		main commune) for this earthquake
+		main commune) for this earthquake.
+		This is a wrapper for :meth:`get_aggregated_traditional_macro_info`
+		"""
+		kwargs = locals().copy()
+		kwargs.pop('self')
+		return self.get_aggregated_traditional_macro_info(data_type='official', **kwargs)
 
-		:param min_or_max:
-			str, one of 'min', 'mean' or 'max' to select between
-			intensity_min and intensity_max values in database
-			(default: 'max')
-		:param min_val:
-			float, minimum intensity to return
-			(default: 1)
-		:param group_by_main_village:
-			bool, whether or not to aggregate the results by main village
+	def get_aggregated_historical_macro_info(self, id_com=None, min_or_max='max',
+			group_by_main_commune=False, agg_method="mean", min_fiability=20,
+			verbose=False):
+		"""
+		Get historical macroseismic information (possibly aggregated by
+		main commune) for this earthquake.
+		This is a wrapper for :meth:`get_aggregated_traditional_macro_info`
+		"""
+		kwargs = locals().copy()
+		kwargs.pop('self')
+		return self.get_aggregated_traditional_macro_info(data_type='historical', **kwargs)
+
+	def get_traditional_macroseismic_info(self, id_com=None, data_type='',
+					group_by_main_commune=False, min_fiability=20,
+					verbose=False, errf=None):
+		"""
+		Get traditional macroseismic information for this earthquake
+
+		:param id_com:
+			int, commune ID
+			or list of ints
+			(default: None)
+		:param data_type:
+			str, type of macroseismic data: '', 'official' or 'historical'
+			(default: '')
+		:param group_by_main_commune:
+			bool, whether or not to group the results by main village
 			(default: False)
-		:param agg_method:
-			str, aggregation function to use if :param:`group_by_main_village`
-			is True, one of "minimum", "maximum" or "average"/"mean"
-			(default: "average")
 		:param min_fiability:
 			float, minimum fiability of enquiry
 			(default: 20.)
 		:param verbose:
 			Bool, if True the query string will be echoed to standard output
+		:param errf:
+			File object, where to print errors
 
 		:return:
-			dict mapping commune IDs to instances of :class:`MacroseismicInfo`
+			dict, mapping commune IDs (if :param:`id_earth` is not None) or
+			earthquake IDs (if :param:`id_earth` is None) to lists of
+			database records (1 or more depending on :param:`group_by_main_commune`)
 		"""
-		from .seismodb import query_official_macro_catalog
-		return query_official_macro_catalog(self.ID, min_or_max=min_or_max, min_val=min_val,
-			group_by_main_village=group_by_main_village, agg_method=agg_method,
-			min_fiability=min_fiability, verbose=verbose)
+		kwargs = locals().copy()
+		kwargs.pop('self')
+		from .seismodb import query_traditional_macro_catalog
+		return query_traditional_macro_catalog(self.ID, **kwargs)
 
-	def get_macroseismic_enquiries(self, min_fiability=20, min_location_quality=6,
+	def get_official_macroseismic_info(self, id_com=None,
+					group_by_main_commune=False, min_fiability=20,
+					verbose=False, errf=None):
+		"""
+		Get official macroseismic information for this earthquake
+		This is a wrapper for :meth:`get_traditional_macroseismic_info`
+		"""
+		kwargs = locals().copy()
+		kwargs.pop('self')
+		return self.get_traditional_macroseismic_info(data_type='official', **kwargs)
+
+	def get_historical_macroseismic_info(self, id_com=None,
+					group_by_main_commune=False, min_fiability=20,
+					verbose=False, errf=None):
+		"""
+		Get historical macroseismic information for this earthquake
+		This is a wrapper for :meth:`get_traditional_macroseismic_info`
+		"""
+		kwargs = locals().copy()
+		kwargs.pop('self')
+		return self.get_traditional_macroseismic_info(data_type='historical', **kwargs)
+
+	def get_isoseismal_macroseismic_info(self, group_by_main_commune=False,
+										as_points=True):
+		"""
+		Get isoseismal macroseismic information for this earthquake
+
+		:param main_communes:
+			bool, whether or not to use main communes
+			(default: False)
+		:param as_points:
+			bool, whether  points (True) or polygons (False) should
+			be used for testing whether communes are within a particular
+			isoseismal
+			(default: True)
+
+		:return:
+			dict, mapping commune IDs to intensities
+		"""
+		from ..macro.isoseismal import get_commune_intensities_from_isoseismals
+		return get_commune_intensities_from_isoseismals(self.ID,
+						main_communes=group_by_main_commune, as_points=as_points)
+
+	def get_online_macro_enquiries(self, min_fiability=20, min_location_quality=6,
 									verbose=False):
 		"""
-		Get all macroseismic enquiries for this earthquake
+		Get all online macroseismic enquiries for this earthquake
 
 		:param min_fiability:
 			float, minimum fiability of enquiry
@@ -132,15 +267,15 @@ class ROBLocalEarthquake(LocalEarthquake):
 		:return:
 			instance of :class:`MacroseismicEnquiryEnsemble`
 		"""
-		from .seismodb import query_web_macro_enquiries
-		ensemble = query_web_macro_enquiries(self.ID, min_fiability=min_fiability,
+		from .seismodb import query_online_macro_catalog
+		ensemble = query_online_macro_catalog(self.ID, min_fiability=min_fiability,
 										min_location_quality=min_location_quality,
 										verbose=verbose)
 		return ensemble
 
-	def get_num_macroseismic_enquiries(self, min_fiability=20):
+	def get_num_online_macro_enquiries(self, min_fiability=20):
 		"""
-		Count number of macroseismic enquiries for a particular event.
+		Count number of online macroseismic enquiries for a particular event.
 
 		:param min_fiability:
 			float, minimum fiability of enquiries
@@ -149,13 +284,14 @@ class ROBLocalEarthquake(LocalEarthquake):
 		:return:
 			int, number of enquiries
 		"""
-		from .seismodb import get_num_macroseismic_enquiries
-		[num_enquiries] = get_num_macroseismic_enquiries([self.ID], min_fiability)
+		from .seismodb import get_num_online_macro_enquiries
+		[num_enquiries] = get_num_online_macro_enquiries([self.ID], min_fiability)
 		return num_enquiries
 
-	def get_Imax_web(self, min_replies=3, min_fiability=20, filter_floors=(0, 4),
+	def get_Imax_online(self, min_replies=3, min_fiability=20.0, filter_floors=(0,4),
+					aggregate_by='commune', agg_method="mean", fix_records=True,
 					include_other_felt=True, include_heavy_appliance=False,
-					remove_outliers=(2.5, 97.5)):
+					remove_outliers=(2.5, 97.5), verbose=False):
 		"""
 		Report maximum intensity from online enquiry in any commune
 		for this earthquake
@@ -163,63 +299,129 @@ class ROBLocalEarthquake(LocalEarthquake):
 		:param min_replies:
 		:param min_fiability:
 		:param filter_floors:
+		:param aggregate_by:
+		:param agg_method:
+		:param fix_records:
 		:param include_other_felt:
 		:param include_heavy_appliance:
 		:param remove_outliers:
-			see :meth:`get_macroseismic_data_aggregated_web`
+		:param verbose:
+			see :meth:`get_aggregated_online_macro_info`
 
 		:return:
 			float, Imax
 		"""
+		kwargs = locals().copy()
+		kwargs.pop('self')
+		macro_info_col = self.get_aggregated_online_macro_info(query_info="cii",
+																**kwargs)
+		if len(macro_info_col):
+			_, Imax = macro_info_col.Iminmax()
+		else:
+			Imax = 0
+
 		# TODO: add recalc argument and use get_aggregated_info method of dyfi
-		dyfi = self.get_macroseismic_enquiries(min_fiability)
-		Imax = 0
-		if len(dyfi):
-			for id_com, com_ensemble in dyfi.aggregate_by_commune().items():
-				if len(com_ensemble) >= min_replies:
-					I = com_ensemble.calc_mean_cii(filter_floors=filter_floors,
-							include_other_felt=include_other_felt,
-							include_heavy_appliance=include_heavy_appliance,
-							remove_outliers=remove_outliers)
-					if I and I > Imax:
-						Imax = I
+		#dyfi = self.get_macroseismic_enquiries(min_fiability)
+		#Imax = 0
+		#if len(dyfi):
+		#	for id_com, com_ensemble in dyfi.aggregate_by_commune().items():
+		#		if len(com_ensemble) >= min_replies:
+		#			I = com_ensemble.calc_mean_cii(filter_floors=filter_floors,
+		#					include_other_felt=include_other_felt,
+		#					include_heavy_appliance=include_heavy_appliance,
+		#					remove_outliers=remove_outliers)
+		#			if I and I > Imax:
+		#				Imax = I
 		return Imax
 
-	def get_Imax_official(self, min_or_max='max', min_fiability=20):
+	def get_Imax_traditional(self, data_type='', min_or_max='max',
+			min_fiability=20, aggregate_by="commune", agg_method="mean",
+			verbose=False):
 		"""
-		Report maximum intensity in any commune for this earthquake.
+		Report maximum traditional intensity in any commune for this earthquake.
 
+		:param data_type:
 		:param min_or_max:
 		:param min_fiability:
-			see :meth:`get_macroseismic_data_official`
+		:param aggregate_by:
+		:param agg_method:
+		:param verbose:
+			see :meth:`get_aggregated_traditional_macro_info`
 
 		:return:
 			float, Imax
 		"""
-		Imax = 0
-		macro_recs = self.get_macroseismic_data_aggregated_official(min_or_max=min_or_max,
-													min_val=0, min_fiability=min_fiability)
-		for id_com, macro_info in macro_recs.items():
-			if macro_info.I > Imax:
-				Imax = macro_info.I
+		kwargs = locals().copy()
+		kwargs.pop('self')
+		macro_info_col = self.get_aggregated_traditional_macro_info(**kwargs)
+		if len(macro_info_col):
+			_, Imax = macro_info_col.Iminmax()
+		else:
+			Imax = 0
 		return Imax
 
-	def get_Imax(self, min_or_max='max', min_replies=3, min_fiability=20,
-				filter_floors=(0, 4), include_other_felt=True,
-				include_heavy_appliance=False, remove_outliers=(2.5, 97.5)):
+	def get_Imax_official(self, min_or_max='max', min_fiability=20,
+				aggregate_by="commune", agg_method="mean", verbose=False):
 		"""
-		Report maximum intensity (online or official) in any commune
+		Report maximum official intensity in any commune for this earthquake.
+		This is a wrapper for :meth:`get_Imax_traditional`
+		"""
+		kwargs = locals().copy()
+		kwargs.pop('self')
+		return self.get_Imax_traditional(data_type='official', **kwargs)
+
+	def get_Imax_historical(self, min_or_max='max', min_fiability=20,
+				aggregate_by="commune", agg_method="mean", verbose=False):
+		"""
+		Report maximum historical intensity in any commune for this earthquake.
+		This is a wrapper for :meth:`get_Imax_traditional`
+		"""
+		kwargs = locals().copy()
+		kwargs.pop('self')
+		return self.get_Imax_traditional(data_type='historical', **kwargs)
+
+	def get_Imax_isoseismal(self):
+		"""
+		Report maximum isoseismal intensity for this earthquake
+		"""
+		import numpy as np
+		from ..macro.isoseismal import read_isoseismals
+
+		pg_data = read_isoseismals(self.ID, filled=True)
+		if pg_data:
+			return np.max(pg_data.values['Intensity'])
+		else:
+			return 0
+
+	def get_Imax(self, min_fiability=20,
+				min_or_max='mean',
+				min_replies=3, filter_floors=(0, 4),
+				agg_method_online='mean', fix_records=True, include_other_felt=True,
+				include_heavy_appliance=False, remove_outliers=(2.5, 97.5),
+				by_main_commune=False, agg_subcommunes='mean',
+				verbose=False):
+		"""
+		Report maximum intensity (online or traditional) in any commune
 		for this earthquake
+
+		See :meth:`get_Imax_online` and :meth:`get_Imax_traditional`
 
 		:return:
 			float, Imax
 		"""
-		## Note: same min_fiability for official and web
-		Imax_official = self.get_Imax_official(min_or_max=min_or_max,
-												min_fiability=min_fiability)
-		Imax_web = self.get_Imax_web(min_replies, min_fiability, filter_floors,
-					include_other_felt, include_heavy_appliance, remove_outliers)
-		return max(Imax_official, Imax_web)
+		## Note: same min_fiability for traditional and online
+		aggregate_by = {True: 'main commune', False: 'commune'}[by_main_commune]
+		Imax_traditional = self.get_Imax_traditional(min_fiability=min_fiability,
+								min_or_max=min_or_max, aggregate_by=by_main_commune,
+								agg_method=agg_subcommunes, verbose=verbose)
+		Imax_online = self.get_Imax_online(min_replies=min_replies,
+							min_fiability=min_fiability, filter_floors=filter_floors,
+							aggregate_by=aggregate_by, agg_method=agg_method_online,
+							fix_records=fix_records, include_other_felt=include_other_felt,
+							include_heavy_appliance=include_heavy_appliance,
+							remove_outliers=remove_outliers)
+
+		return max(Imax_traditional, Imax_online)
 
 	def get_focal_mechanism(self, verbose=False):
 		"""
