93f4d218951ffba0ca65192f4aed3159285885aa
krisvanneste <kris.vanneste@oma.be>
2019-06-09 10:30:49 +0000

    Added 'fill_colors', 'xticks', 'xticklabels', 'yticks', 'yticklabels', 'ax_label_fontsize', 'tick_label_fontsize' and 'style_sheet' arguments to plot_xy function. Completed docstring to plot_xy function. Use default colors from specified style_sheet for 'colors' argument in plot_xy function. Implement axis scaling using set_xscale and set_yscale functions in matplotlib, rather than using different plot functions. Replaced all generic arguments in plot_cumulated_moment function with **kwargs.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/eqcatalog/trunk@5505 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- plot/plot_catalog.py
+++ plot/plot_catalog.py
@@ -11,6 +11,7 @@ import numpy as np
 import pylab
 import matplotlib.dates as mdates
 from matplotlib.ticker import MultipleLocator, MaxNLocator
+from matplotlib.font_manager import FontProperties
 
 import eqcatalog.time_functions_np as tf
 
@@ -19,24 +20,139 @@ MPL_FONT_SIZES = ['xx-small', 'x-small', 'small', 'medium',
 				'large', 'x-large', 'xx-large']
 
 
-def plot_xy(datasets, xscaling='lin', yscaling='lin',
-			colors=[], linewidths=[], linestyles=[], labels=[],
-			markers=[], marker_sizes=[], marker_intervals=[],
-			marker_edge_colors=[], marker_fill_colors=[], marker_edge_widths=[],
+def plot_xy(datasets,
+			colors=[], fill_colors=[], linewidths=[1], linestyles=['-'], labels=[],
+			markers=[], marker_sizes=[6], marker_intervals=[],
+			marker_edge_colors=['k'], marker_fill_colors=[], marker_edge_widths=[1],
+			xscaling='lin', yscaling='lin',
 			xmin=None, xmax=None, ymin=None, ymax=None,
-			xlabel='', ylabel='', title='', grid=False,
-			label_font_size='medium', title_font_size='large',
-			legend_font_size='medium', legend_location=0,
+			xlabel='', ylabel='', ax_label_fontsize='large',
+			xticks=None, xticklabels=None, yticks=None, yticklabels=None,
+			tick_label_fontsize='medium',
+			title='', title_fontsize='large',
+			legend_location=0, legend_fontsize='medium',
+			grid=False, style_sheet='classic',
 			fig_filespec=None, dpi=300, ax=None):
 	"""
-	Generic function to plot (X, Y) data sets
+	Generic function to plot (X, Y) data sets (lines, symbols and/or polygons)
 
 	:param datasets:
 		list with (x, y) array tuples
+	:param colors:
+		list of line colors to cycle over for each dataset
+		(default: [], will use default colors for :param:`style_sheet`)
+	:param fill_colors:
+		list of fill colors to cycle over for each dataset
+		(default: [], will not apply fill color)
+	:param linewidths:
+		list of line widths to cycle over for each dataset
+		(default: [1])
+	:param linestyles:
+		list of line styles to cycle over for each dataset
+		(default: ['-'])
+	:param labels:
+		list of labels to cycle over for each dataset
+		(default: [], will not label curves)
+	:param markers:
+		list of marker symbols to cycle over for each dataset
+		(default: [], will not draw markers)
+	:param marker_sizes:
+		list of marker sizes to cycle over for each dataset
+		(default: [6])
+	:param marker_intervals:
+		(default: [], will draw marker for each datapoint)
+	:param marker_edge_colors:
+		list of marker line colors to cycle over for each dataset
+		(default: ['k'])
+	:param marker_fill_colors:
+		list of marker fill colors to cycle over for each dataset
+		(default: [], will use colors defined in :param:`colors`)
+	:param marker_edge_widths:
+		list of marker line widths to cycle over for each dataset
+		(default: [1])
+	:param xscaling:
+		str, scaling to use for X axis ('lin' or 'log')
+		(default: 'lin')
+	:param yscaling:
+		str, scaling to use for Y axis ('lin' or 'log')
+		(default: 'lin')
+	:param xmin:
+		float, start value for X axis
+		(default: None, let matplotlib decide)
+	:param xmax:
+		float, end value for X axis
+		(default: None, let matplotlib decide)
+	:param ymin:
+		float, start value for Y axis
+		(default: None, let matplotlib decide)
+	:param ymax:
+		float, end value for Y axis
+		(default: None, let matplotlib decide)
+	:param xlabel:
+		str, label for X axis
+		(default: '')
+	:param ylabel:
+		str, label for Y axis
+		(default: '')
+	:param ax_label_fontsize:
+		int or str, font size to use for axis labels
+		(default: 'large')
+	:param xticks:
+		list or array, X axis tick positions
+		(default: None, let matplotlib decide)
+	:param xticklabels:
+		list of labels corresponding to X axis ticks
+		(default: None, let matplotlib decide)
+	:param yticks:
+		list or array, Y axis tick positions
+		(default: None, let matplotlib decide)
+	:param yticklabels:
+		list of labels corresponding to Y axis ticks
+		(default: None, let matplotlib decide)
+	:param tick_label_fontsize:
+		int or str, font size to use for axis tick labels
+		(default: 'medium')
+	:param title:
+		str, plot title
+		(default: '')
+	:param title_fontsize:
+		str, font size to use for plot title
+		(default: 'large')
+	:param legend_location:
+		int or str, matplotlib spec where to plot legend
+		(default: 0 = auto-detect optimal position)
+	:param legend_fontsize:
+		int or str, font size to use for legend labels
+		If not specified, will use the value of :param:`tick_label_fontsize`
+		(default: 'medium')
+	:param grid:
+		bool, whether or not to show grid lines
+		(default: False)
+	:param style_sheet:
+		str, matplotlib style sheet to apply to plot
+		See matplotlib.style.available for availabel style sheets
+		(default: 'classic')
+	:param fig_filespec:
+		str, full path to output file
+		If None, will plot on screen
+		If 'hold', plotting is deferred
+		(default: None)
+	:param dpi:
+		int, resolution of plot,
+		only applies if :param:`fig_filespec` is set to output file
+		(default: 300)
+	:param ax:
+		matplotlib Axes instance, in which plot will be drawn
+		If specified, :param:`fig_filespec` will be overridden with 'hold'
+		(default: None, will generate new Axes instance)
+
+	:return:
+		matplotlib Axes instance if :param:`fig_filespec` is set to
+		'hold', else None
 	"""
-	# TODO: xticks, xtick_labels, yticks, ytick_labels, marker_edge_widths...
 	from itertools import cycle
-	from matplotlib.font_manager import FontProperties
+
+	pylab.style.use(style_sheet)
 
 	if ax is None:
 		#ax = pylab.axes()
@@ -46,7 +162,11 @@ def plot_xy(datasets, xscaling='lin', yscaling='lin',
 
 	## markers, colors, linewidhts, linestyles, labels, etc.
 	if not colors:
-		colors = ["r", "g", "b", "c", "m", "k"]
+		#colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
+		#colors = 'bgrcmyk'
+		colors = pylab.rcParams['axes.prop_cycle'].by_key()['color']
+	if not fill_colors:
+		fill_colors = [None]
 	if not linewidths:
 		linewidths = [1]
 	if not linestyles:
@@ -67,6 +187,7 @@ def plot_xy(datasets, xscaling='lin', yscaling='lin',
 		labels = ['_nolegend_']
 
 	colors = cycle(colors)
+	fill_colors = cycle(fill_colors)
 	linewidths = cycle(linewidths)
 	linestyles = cycle(linestyles)
 	markers = cycle(markers)
@@ -90,6 +211,7 @@ def plot_xy(datasets, xscaling='lin', yscaling='lin',
 
 	for (x, y) in datasets:
 		color = colors.next()
+		fill_color = fill_colors.next()
 		linewidth = linewidths.next()
 		linestyle = linestyles.next()
 		marker = markers.next()
@@ -100,25 +222,38 @@ def plot_xy(datasets, xscaling='lin', yscaling='lin',
 		marker_edge_width = marker_edge_widths.next()
 		label = labels.next()
 
-		ax.plot(x, y, marker, color=color, ls=linestyle, lw=linewidth,
+		if fill_color:
+			ax.fill(x, y, facecolor=fill_color, edgecolor=color, lw=linewidth,
+				ls=linestyle, label=label)
+			if marker:
+				ax.plot(x, y, marker, lw=0, ms=marker_size, mec=marker_edge_color,
+				mfc=marker_fill_color, mew=marker_edge_width, markevery=marker_interval,
+				label='_nolegend_')
+		else:
+			ax.plot(x, y, marker, color=color, ls=linestyle, lw=linewidth,
 				ms=marker_size, mec=marker_edge_color, mfc=marker_fill_color,
 				mew=marker_edge_width, markevery=marker_interval, label=label)
 
 	if grid:
 		ax.grid('on')
 
-	legend_font_size = legend_font_size or label_font_size
-	legend_font = FontProperties(size=legend_font_size)
+	## Legend and title
+	legend_fontsize = legend_fontsize or tick_label_fontsize
+	legend_font = FontProperties(size=legend_fontsize)
 	ax.legend(loc=legend_location, prop=legend_font)
-	ax.set_xlabel(xlabel, fontsize=title_font_size)
-	ax.set_ylabel(ylabel, fontsize=title_font_size)
-	ax.set_title(title, fontsize=title_font_size)
+	ax.set_title(title, fontsize=title_fontsize)
 
+	## Axis labels
+	ax.set_xlabel(xlabel, fontsize=ax_label_fontsize)
+	ax.set_ylabel(ylabel, fontsize=ax_label_fontsize)
+
+	## Axis scaling
 	xscaling = {'lin': 'linear', 'log': 'log'}[xscaling]
 	ax.set_xscale(xscaling)
 	yscaling = {'lin': 'linear', 'log': 'log'}[yscaling]
 	ax.set_yscale(yscaling)
 
+	## Axis limits
 	_xmin, _xmax = ax.get_xlim()
 	xmin = xmin or _xmin
 	xmax = xmax or _xmax
@@ -129,11 +264,21 @@ def plot_xy(datasets, xscaling='lin', yscaling='lin',
 	ymax = ymax or _ymax
 	ax.set_ylim(ymin, ymax)
 
+	## Ticks and tick labels
+	if xticks is not None:
+		ax.set_xticks(xticks)
+	if xticklabels:
+		ax.set_xticklabels(xticklabels)
+	if yticks is not None:
+		ax.set_yticks(yticks)
+	if yticklabels:
+		ax.set_yticklabels(yticklabels)
+
 	for label in ax.get_xticklabels() + ax.get_yticklabels():
-		label.set_size(label_font_size)
+		label.set_size(tick_label_fontsize)
 
 	if fig_filespec == "hold":
-		return
+		return ax
 	elif fig_filespec:
 		#default_figsize = pylab.rcParams['figure.figsize']
 		#if fig_width:
@@ -144,14 +289,11 @@ def plot_xy(datasets, xscaling='lin', yscaling='lin',
 	else:
 		pylab.show()
 
+	pylab.style.use('default')
 
-def plot_cumulated_moment(catalogs, colors=[], linewidths=[], linestyles=[], markers=[],
-						marker_sizes=[], marker_edge_colors=[], marker_fill_colors=[],
-						labels=[], start_date=None, end_date=None, time_unit='Y',
-						Mrelation="default", M0max=None, grid=True, title='',
-						label_font_size='medium', title_font_size='large',
-						legend_font_size='medium', legend_location=2,
-						fig_filespec=None, dpi=300, ax=None):
+
+def plot_cumulated_moment(catalogs, start_date=None, end_date=None, time_unit='Y',
+						Mrelation="default", M0max=None, **kwargs):
 	"""
 	:param time_unit:
 		'Y', 'D', 'h', 'm', 's'
@@ -171,7 +313,7 @@ def plot_cumulated_moment(catalogs, colors=[], linewidths=[], linestyles=[], mar
 		end_date = tf.as_np_datetime(end_date)
 
 	datasets = []
-	marker_intervals = [2]
+	kwargs['marker_intervals'] = [2]
 	for i, catalog in enumerate(catalogs):
 		if time_unit == 'Y':
 			dates = catalog.get_fractional_years()
@@ -190,26 +332,19 @@ def plot_cumulated_moment(catalogs, colors=[], linewidths=[], linestyles=[], mar
 
 	time_unit_str = {'Y': 'years', 'D': 'days', 'h': 'hours',
 					'm': 'minutes', 's': 'seconds'}[time_unit]
-	xlabel = "Time (%s)" % time_unit_str
-	ylabel = "Seismic Moment (N.m)"
+	kwargs['xlabel'] = "Time (%s)" % time_unit_str
+	kwargs['ylabel'] = "Seismic Moment (N.m)"
 
 	if time_unit == 'Y':
-		xmin = tf.to_fractional_year(start_date)
-		xmax = tf.to_fractional_year(end_date)
+		kwargs['xmin'] = tf.to_fractional_year(start_date)
+		kwargs['xmax'] = tf.to_fractional_year(end_date)
 	else:
-		xmin = 0
-		xmax = tf.timespan(start_date, end_date, time_unit)
+		kwargs['xmin'] = 0
+		kwargs['xmax'] = tf.timespan(start_date, end_date, time_unit)
 
-	ymax = M0max
+	kwargs['ymax'] = M0max
 
-	marker_edge_colors = colors[:]
-	marker_fill_colors = ['None']
+	kwargs['marker_edge_colors'] = kwargs.get('colors', [])[:]
+	kwargs['marker_fill_colors'] = ['None']
 
-	return plot_xy(datasets, colors=colors, linewidths=linewidths,
-			linestyles=linestyles, labels=labels, markers=markers,
-			marker_sizes=marker_sizes, marker_intervals=marker_intervals,
-			marker_edge_colors=marker_edge_colors, marker_fill_colors=marker_fill_colors,
-			xmin=xmin, xmax=xmax, ymax=ymax, legend_location=legend_location,
-			xlabel=xlabel, ylabel=ylabel, title=title, grid=grid,
-			label_font_size=label_font_size, title_font_size=title_font_size,
-			fig_filespec=fig_filespec, dpi=dpi, ax=ax)
+	return plot_xy(datasets, **kwargs)
