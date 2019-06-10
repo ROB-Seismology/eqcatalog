f84ff7511a8241996a8b041e6087c417e83483d7
krisvanneste <kris.vanneste@oma.be>
2019-06-10 21:58:07 +0000

    Added plot_magnitude_time function.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/eqcatalog/trunk@5510 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- plot/plot_catalog.py
+++ plot/plot_catalog.py
@@ -1,4 +1,6 @@
+# -*- coding: iso-Latin-1 -*-
 """
+Various earthquake catalog plotting functions
 """
 
 from __future__ import absolute_import, division, print_function, unicode_literals
@@ -8,9 +10,111 @@ from builtins import int
 import os
 
 import numpy as np
+import pylab
+import matplotlib
+import matplotlib.dates as mdates
 
 import eqcatalog.time_functions_np as tf
 from .plot_generic import plot_xy
+
+
+def plot_magnitude_time(catalogs, Mtype, Mrelation, time_unit='Y',
+						Mrange=(None, None), start_date=None, end_date=None,
+						markers=['o'], marker_sizes=[8], colors=[], fill_colors=[],
+						labels=[],
+						completeness=None, completeness_color="r",
+						lang='en', **kwargs):
+	"""
+	"""
+	if start_date is None:
+		start_date = np.min([cat.start_date for cat in catalogs])
+	else:
+		if isinstance(start_date, (int)):
+			start_date = str(start_date)
+		start_date = tf.as_np_datetime(start_date)
+	if end_date is None:
+		end_date = np.max([cat.end_date for cat in catalogs])
+	else:
+		if isinstance(end_date, (int)):
+			end_date = str(end_date)
+		end_date = tf.as_np_datetime(end_date)
+
+	if time_unit == 'dt':
+		kwargs['xmin'] = pylab.date2num(tf.to_py_datetime(start_date))
+		kwargs['xmax'] = pylab.date2num(tf.to_py_datetime(end_date))
+	elif time_unit == 'Y':
+		kwargs['xmin'] = tf.to_fractional_year(start_date)
+		kwargs['xmax'] = tf.to_fractional_year(end_date)
+	else:
+		kwargs['xmin'] = 0
+		kwargs['xmax'] = tf.timespan(start_date, end_date, time_unit)
+
+	min_mag, max_mag = Mrange
+	if min_mag is not None:
+		kwargs['xmin'] = min_mag
+	if max_mag is not None:
+		kwargs['xmax'] = max_mag
+
+	## Catalogs
+	datasets = []
+	for i, catalog in enumerate(catalogs):
+		mags = catalog.get_magnitudes(Mtype, Mrelation)
+		if time_unit == 'dt':
+			dates = [tf.to_py_datetime(dt) for dt in catalog.get_datetimes()]
+		elif time_unit == 'Y':
+			dates = catalog.get_fractional_years()
+		else:
+			dates = tf.timespan(start_date, catalog.get_datetimes(), time_unit)
+		datasets.append((dates, mags))
+		if isinstance(labels, list) and len(labels) <= i:
+			labels.append(catalog.name)
+
+	kwargs['marker_edge_colors'] = colors or list('bgrcmyk')
+	kwargs['colors'] = ['None'] * len(datasets)
+	kwargs['marker_fill_colors'] = fill_colors
+	kwargs['fill_colors'] = []
+	kwargs['markers'] = markers
+	kwargs['marker_sizes'] = marker_sizes
+
+	## Completeness
+	# TODO: implement completeness dates rather than years
+	if completeness:
+		clabel = {"en": "Completeness magnitude",
+				"nl": "Compleetheidsmagnitude",
+				"fr": u"Magnitude de complétude",
+				"nlfr": u"Compleetheid / Complétude"}[lang]
+		x, y = completeness.min_dates, completeness.min_mags
+		#x = np.concatenate([x[:1], np.repeat(x[1:], 2), [tf.to_fractional_year(end_date)]])
+		x = np.concatenate([x[:1], np.repeat(x[1:], 2), [tf.to_py_datetime(end_date)]])
+		y = np.repeat(y, 2)
+		datasets.append((x, y))
+
+		if not isinstance(labels, list):
+			labels = ['_nolegend_'] * len(catalogs)
+		labels.append(clabel)
+		kwargs['colors'].append(completeness_color)
+		kwargs['markers'].append('')
+		kwargs['marker_edge_colors'].append('None')
+		kwargs['marker_sizes'].append(0)
+		kwargs['linewidths'] = [0] * len(catalogs) + [2]
+
+	kwargs['labels'] = labels
+
+	if not kwargs['xlabel']:
+		kwargs['xlabel'] = {"en": "Date",
+							"nl": "Datum",
+							"fr": "Date",
+							"nlfr": "Datum / Date"}[lang]
+	kwargs['ylabel'] = kwargs.get('ylabel', "Magnitude ($M_%s$)" % Mtype[1])
+	kwargs['xtick_interval'] = kwargs.get('xtick_interval', (mdates.AutoDateLocator(),
+																mdates.AutoDateLocator()))
+	kwargs['ytick_interval'] = kwargs.get('ytick_interval', (1, 0.1))
+	kwargs['xgrid'] = kwargs.get('xgrid', 1)
+	kwargs['ygrid'] = kwargs.get('ygrid', 1)
+
+	return plot_xy(datasets, **kwargs)
+
+plot_magnitude_time.__doc__ += plot_xy.__doc__.split("\n", 4)[4]
 
 
 def plot_cumulated_moment(catalogs, start_date=None, end_date=None, time_unit='Y',
@@ -37,7 +141,6 @@ def plot_cumulated_moment(catalogs, start_date=None, end_date=None, time_unit='Y
 		float, maximum moment value in Y axis
 		(default: None)
 	"""
-
 	if start_date is None:
 		start_date = np.min([cat.start_date for cat in catalogs])
 	else:
@@ -52,7 +155,6 @@ def plot_cumulated_moment(catalogs, start_date=None, end_date=None, time_unit='Y
 		end_date = tf.as_np_datetime(end_date)
 
 	datasets = []
-	kwargs['marker_intervals'] = [2]
 	for i, catalog in enumerate(catalogs):
 		if time_unit == 'Y':
 			dates = catalog.get_fractional_years()
@@ -68,6 +170,8 @@ def plot_cumulated_moment(catalogs, start_date=None, end_date=None, time_unit='Y
 		dates2 = np.repeat(dates, 2)
 
 		datasets.append((dates2, M0_cumul2))
+
+	kwargs['marker_intervals'] = [2]
 
 	time_unit_str = {'Y': 'years', 'D': 'days', 'h': 'hours',
 					'm': 'minutes', 's': 'seconds'}[time_unit]
