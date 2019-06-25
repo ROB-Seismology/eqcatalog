09e1d31089f6c0a9aea4a668e8d00fbe2bfacfee
krisvanneste <kris.vanneste@oma.be>
2019-06-25 12:03:46 +0000

    Pass 'default_completeness' property in copy method of EQCatalog. Set 'default_Mrelations' property of returned catalog in subselect and subselect_completeness methods ot EQCatalog to the relation that was actually used.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/eqcatalog/trunk@5573 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- eqcatalog.py
+++ eqcatalog.py
@@ -187,9 +187,11 @@ class EQCatalog(object):
 		region = self.region
 		name = self.name
 		default_Mrelations = self.default_Mrelations
+		default_completeness = self.default_completeness
 		return self.__class__(eq_list, start_date=start_date, end_date=end_date,
 							region=region, name=name,
-							default_Mrelations=default_Mrelations)
+							default_Mrelations=default_Mrelations,
+							default_completeness=default_completeness)
 
 	@property
 	def lons(self):
@@ -1370,7 +1372,7 @@ class EQCatalog(object):
 
 		return EQCatalog(eq_list, start_date=start_date, end_date=end_date,
 						region=region, name=catalog_name,
-						default_Mrelations=self.default_Mrelations,
+						default_Mrelations={Mtype: Mrelation},
 						default_completeness=self.default_completeness)
 
 	def subselect_distance(self, point, distance, catalog_name=""):
@@ -1609,6 +1611,7 @@ class EQCatalog(object):
 
 		## Select magnitudes according to completeness criteria
 		if completeness:
+			Mrelation = Mrelation or self.default_Mrelations[Mtype]
 			eq_list = []
 			for eq in self.eq_list:
 				M = eq.get_or_convert_mag(Mtype, Mrelation)
@@ -1625,7 +1628,7 @@ class EQCatalog(object):
 			catalog_name = self.name + " (completeness-constrained)"
 		return EQCatalog(eq_list, start_date=start_date, end_date=end_date,
 						region=self.region, name=catalog_name,
-						default_Mrelations=self.default_Mrelations,
+						default_Mrelations={Mtype: Mrelation},
 						default_completeness=completeness)
 
 	def split_completeness(self,
