cca8353de7a79669c18df8fb132f9ef7ec743f58
krisvanneste <kris.vanneste@oma.be>
2019-12-06 14:01:57 +0000

    Updated export_catalog.py script in frame of study for VPO.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/eqcatalog/trunk@5936 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- app/export_catalog.py
+++ app/export_catalog.py
@@ -6,6 +6,9 @@ import os
 import numpy as np
 import eqcatalog
 
+
+out_folder = r"C:\Temp"
+
 end_date = 2018
 region = (1, 8, 49, 52)
 
@@ -55,8 +58,9 @@ Mtype = 'MW'
 Mrelation = {}
 """
 
-## VPO
-start_date = 1350
+## VPO (tectonic)
+out_folder = r"E:\Home\_kris\Projects\2019 - VPO Balmatt"
+start_date = '1350-01-01'
 end_date = '2019-10-31'
 #region = (1.25, 8.75, 49.15, 53.3)
 region = (1, 8, 49, 52)
@@ -64,11 +68,18 @@ event_type = 'ke'
 Mmin = None
 Mtype = 'MW'
 Mrelation = {}
+catalog_name = 'ROB tectonic earthquake catalog'
+
+## VPO (all instrumental)
+start_date = '1910-01-01'
+event_type = 'ke,se,ki,si,qb,sqb,km,sm,kr,sr,cb,scb,kx,sx'
+catalog_name = 'ROB instrumental event catalog'
+
 
 
 raw_cat = eqcatalog.rob.query_local_eq_catalog(start_date=start_date, end_date=end_date,
-											region=region, Mmin=0, event_type=event_type)
-
+											region=region, Mmin=-1, event_type=event_type,
+											verbose=True)
 ## Add converted moment magnitude column for subselecting
 moment_mags = raw_cat.get_magnitudes(Mtype, Mrelation)
 for i in range(len(raw_cat)):
@@ -83,6 +94,7 @@ if Mmin:
 raw_cat.print_info()
 
 cat = raw_cat.get_sorted()
+cat.name = catalog_name
 #cat1 = raw_cat.subselect(Mmin=Mmin, Mtype='ML', Mrelation={})
 #cat2 = raw_cat.subselect(Mmin=Mmin, Mtype='MS', Mrelation={})
 #cat = cat1 + cat2
@@ -91,11 +103,14 @@ cat = raw_cat.get_sorted()
 #cat2.print_list()
 
 ## Export as CSV
-csv_file = "C:\\Temp\\ROB_catalog_%s_%d-%s.csv" % (event_type, start_date, end_date)
+if ',' in event_type:
+	event_type = 'all'
+csv_file = os.path.join(out_folder, "ROB_catalog_%s_%s-%s.csv" % (event_type,
+						start_date.replace('-', ''), end_date.replace('-', '')))
 columns = ['ID', 'date', 'time', 'name', 'lon', 'lat', 'depth',
 			'ML', 'MS', 'MW', 'intensity_max',
 			'errt', 'errh', 'errz', 'errM']
-#cat.export_csv(csv_file, columns=columns)
+cat.export_csv(csv_file, columns=columns)
 
 ## Export as Shapefile
 gis_file = os.path.splitext(csv_file)[0] + '.SHP'
@@ -105,6 +120,9 @@ cat.export_gis('ESRI Shapefile', gis_file, columns=columns, replace_null_values=
 
 ## Export as KML
 kml_file = os.path.splitext(csv_file)[0] + '.KML'
-#cat.export_kml(kml_file, time_folders=True, color_by_depth=False, columns=columns)
+if event_type == 'all':
+	cat.export_kml(kml_file, folders='event_type', color_by_depth=False, columns=columns + ['event_type'])
+else:
+	cat.export_kml(kml_file, folders='time', color_by_depth=False, columns=columns)
 
 #cat.plot_map(Mtype='MWc', Mrelation=eqcatalog.msc.IdentityMSC())
