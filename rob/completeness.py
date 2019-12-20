834070513110bbc964a17b4b0c4086592bd206d7
krisvanneste <kris.vanneste@oma.be>
2019-12-20 14:32:00 +0000

    Added Completeness_MW_201303a_ext and Completeness_MW_201303b_ext completeness definitions, extending Completeness_MW_201303a/b with an additional completeness date (1996).
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/eqcatalog/trunk@5985 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- rob/completeness.py
+++ rob/completeness.py
@@ -19,8 +19,13 @@ Completeness_Rosset = Completeness([1350, 1926, 1960, 1985],
 ## Following relation is for MW based on conversion from ML using Ahorner (1983)
 Completeness_MW_201303a = Completeness([1350, 1750, 1860, 1905, 1960, 1985],
 										[5.2, 4.9, 4.5, 4.0, 3.0, 2.2], "MW")
+Completeness_MW_201303a_ext = Completeness([1350, 1750, 1860, 1905, 1960, 1985, 1996],
+										[5.2, 4.9, 4.5, 4.0, 3.0, 2.2, 1.8], "MW")
+
 ## Following relation is for MW based on conversion from ML using Reamer and Hinzen (2004)
 Completeness_MW_201303b = Completeness([1350, 1750, 1860, 1905, 1960, 1985],
 										[5.2, 4.9, 4.5, 3.9, 2.9, 2.0], "MW")
+Completeness_MW_201303b_ext = Completeness([1350, 1750, 1860, 1905, 1960, 1985, 1996],
+										[5.2, 4.9, 4.5, 3.9, 2.9, 2.0, 1.7], "MW")
 
 DEFAULT_COMPLETENESS = Completeness_MW_201303a
